991c93856c2a5969f857f6e64407ebef469c2933
chensl <l1328076914@gmail.com>
2020-08-07 15:18:17 +0800

    no message

--- freeswitch/init.sh
+++ freeswitch/init.sh
@@ -1,2 +1,3 @@
 #！/bin/bash
-docker run -d -it --name=freeswitch -p 15060:15060/udp -p 15080:15080/udp -p 15066:15066 -p 17443:17443 -p 18021:18021 freeswitch /bin/bash
+# docker run -d -it --name=freeswitch -p 5060:5060/udp -p 5080:5080/udp -p 5066:5066 -p 7443:7443 -p 8021:8021 freeswitch /bin/bash
+docker-compose up -d --build
